f34a1d8c8a5ec39b8b3d5e9cef7267a5d6f3e258
Priyanshi Rathore <priyanshirathore0720@gmail.com>
2023-03-23 20:34:26 +0700

    Add files via upload

---
+++ sh.js
@@ -0,0 +1,17 @@
+ let age = prompt("How old are you? Dear");
+ 
+ if(age > 16){
+   alert("Welcome I in the world of SuperHero and recall your childhood memories");
+ }
+ else{
+   alert("Welcome kids in the world of SuperHero")
+ }
+//  setTimeout(function() { alert("my message"); }, time);
+function validateForm() {
+  let x = document.forms["myform"]["fname"].value;
+  if (x == "") {
+    alert("Name must be filled out");
+    return false;
+  }
+}
+// setTimeout(function() { alert("Welcome to the world of SuperHero and recall your childhood memories"); }, 6000);
